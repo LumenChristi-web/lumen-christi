5399db06a2efbd30bddb1849b996720a87dbc810
LumenChristi-web <245850911+LumenChristi-web@users.noreply.github.com>
2025-11-24 11:16:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        green: "#00A991",
+        veryLightGreen: "#ECFDF3",
+        shadowGreen: "#D9F2EF",
+        lightGreen: "#027A48",
+        darkGreen: "#003B33",
+        beige: "#FAF6ED",
+        brown: "#E3DAC5",
+        cream: "#FAF8F2",
+        gray: "#667085",
+        veryDarkBlue: "#1D2939",
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+        RachelBrown: ["RachelBrown", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
